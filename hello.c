03fc2629e6a2e56f5bdf87ee1e2477cb05b7a43f
Victor <victorliu5296@outlook.com>
2024-06-18 09:59:34 -0400

    Hello Linker!

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include "hello.h"
+#include <stdio.h>
+
+void print_hello()
+{
+    printf("Hello, World!\n");
+}
--- main.c
+++ main.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-// #include "hello.h"
+#include "hello.h"
 
 int main()
 {
-    printf("Hello, World!\n");
+    print_hello();
     return 0;
 }
